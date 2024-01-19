9cfddb3b9660cf169d6b17c18419324cd242a3c7
Marcin Wierzbiński <marcin119a@gmail.com>
2024-01-19 13:37:08 +0100

    signal

--- my_script.R
+++ my_script.R
@@ -1,20 +1,15 @@
-setwd('/home/amso/Documents/signature.tools.lib')
-
-devtools::create("signature.tools.lib")
-
-devtools::install()
 
 library(signature.tools.lib)
-signature <- getOrganSignatures('Breast')
-signatures2 <- read.csv("/home/amso/data_comparation/pythonProject/data/WGS_signatures__sigProfiler_SBS_signatures_2019_05_22.modified.csv")
-row.names(signatures2) <- signatures2[,1]
-signatures2 <- signatures2[,-1]
+signatures <- read.csv("/home/amso/Documents/data_comparation/data/WGS_signatures__sigProfiler_SBS_signatures_2019_05_22.modified.csv")
+row.names(signatures) <- signatures[,1]
+signatures <- signatures[,-1]
 
-catalogues <- read.csv("/home/amso/data_comparation/pythonProject/data/M1.csv")
+catalogues <- read.csv("/home/amso/Documents/data_comparation/data/M.csv", sep='\t')
 catalogues <- catalogues[,-1]
+row.names(catalogues) <- catalogues[,1]
 
+fit_results <- Fit(catalogues, signatures2)
 
-Fit(catalogues, signatures2)
 
 #if (!require("BiocManager", quietly = TRUE))
 #    install.packages("BiocManager")
