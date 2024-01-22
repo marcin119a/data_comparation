c36fe57ce06507479ba6972af544dcbdfd4fedc6
Marcin Wierzbiński <marcin119a@gmail.com>
2024-01-22 15:45:21 +0100

    signal experiments

--- my_script.R
+++ my_script.R
@@ -1,17 +1,12 @@
 
 library(signature.tools.lib)
-signatures <- read.csv("/home/amso/Documents/data_comparation/data/WGS_signatures__sigProfiler_SBS_signatures_2019_05_22.modified.csv")
-row.names(signatures) <- signatures[,1]
-signatures <- signatures[,-1]
+signatures <- read.csv("/home/amso/Documents/data_comparation/data/WGS_signatures__sigProfiler_SBS_signatures_2019_05_22.modified.csv", sep='\t')
+row.names(signatures) <- signatures[, 1]
+signatures <- signatures[, -1]
 
 catalogues <- read.csv("/home/amso/Documents/data_comparation/data/M.csv", sep='\t')
+row.names(catalogues) <- catalogues[, 1]
 catalogues <- catalogues[,-1]
-row.names(catalogues) <- catalogues[,1]
 
-fit_results <- Fit(catalogues, signatures2)
-
-
-#if (!require("BiocManager", quietly = TRUE))
-#    install.packages("BiocManager")
-
-#BiocManager::install("BSgenome.Hsapiens.1000genomes.hs37d5")
+fit_results <- Fit(catalogues, signatures)
+save(fit_results, file = "/home/amso/Documents/data_comparation/data/fit_results.RData")
