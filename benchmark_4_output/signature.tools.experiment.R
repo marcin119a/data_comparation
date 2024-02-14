9d2d1e592b38117ed0fe70870e68b8940c46d5eb
Marcin Wierzbiński <marcin119a@gmail.com>
2024-02-14 11:25:34 +0100

    modyfikacja i wizualizacja

--- benchmark_4_output/signature.tools.experiment.R
+++ benchmark_4_output/signature.tools.experiment.R
@@ -5,10 +5,9 @@ row.names(signatures) <- signatures[, 1]
 signatures <- signatures[, -1]
 
 catalogues <- read.csv("/home/amso/Documents/data_comparation/benchmark_4_output/data/data_for_deconstructSigs.csv", sep='\t')
-row.names(catalogues) <- catalogues[, 1]
-catalogues <- catalogues[,-1]
+#catalogues <- catalogues[,-1]
 
 fit_results <- Fit(catalogues, signatures, method='NNLS')
 save(fit_results, file = "/home/amso/Documents/data_comparation/benchmark_4_output/output/fit_results.RData")
 
-write.csv(values, file = "/home/amso/Documents/data_comparation/benchmark_4_output/output/signal.exposures.csv", row.names = FALSE)
+write.csv(fit_results$exposures, file = "/home/amso/Documents/data_comparation/benchmark_4_output/output/signal.exposures.csv", row.names = FALSE)
