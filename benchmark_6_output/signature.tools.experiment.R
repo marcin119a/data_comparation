929f859a74e1e3d82e60548ae6d453604864659e
Marcin Wierzbiński <marcin119a@gmail.com>
2024-02-26 10:30:43 +0100

    new results

--- benchmark_6_output/signature.tools.experiment.R
+++ benchmark_6_output/signature.tools.experiment.R
@@ -7,7 +7,7 @@ signatures <- signatures[, -1]
 catalogues <- read.csv("/home/amso/Documents/data_comparation/benchmark_6_output/data/data_for_deconstructSigs.csv", sep='\t')
 #catalogues <- catalogues[,-1]
 
-fit_results <- Fit(catalogues, signatures, method='NNLS')
+fit_results <- Fit(catalogues, signatures, method='KLD', useBootstrap = TRUE, nboot = 100,   nparallel = 4)
 save(fit_results, file = "/home/amso/Documents/data_comparation/benchmark_6_output/output/fit_results.RData")
-
+#todo boostrap
 write.csv(fit_results$exposures, file = "/home/amso/Documents/data_comparation/benchmark_6_output/output/signal.exposures.csv", row.names = FALSE)
